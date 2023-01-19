49adc3286071c279dd6e4fcf2b89a5520b8e572f
CloudwithNicholas <nicholasc001@hotmail.com>
2023-01-19 15:49:00 +0000

    format code

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,18 @@
 terraform {
- required_providers {
-   aws = {
-     source = "hashicorp/aws"
-     version = "4.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.8.0"
 
-   }
- }
- 
- backend "s3" {
-   region = "eu-west-2"
-   key    = "terraform.tfstate"
- }
+    }
+  }
+
+  backend "s3" {
+    region = "eu-west-2"
+    key    = "terraform.tfstate"
+  }
 }
- 
+
 provider "aws" {
- region = "eu-west-2"
+  region = "eu-west-2"
 }
