9dbe7cbf5bfb03aeb98001a41f3dfaee24293b0d
CloudwithNicholas <nicholasc001@hotmail.com>
2023-01-19 12:38:45 +0000

    edit deploy workflow

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,14 @@
 terraform {
   required_providers {
     aws = {
+
       source  = "hashicorp/aws"
-      version = "4.50.0"
-
+      version = "4.8.0"
     }
-  }
-
-  backend "s3" {
-    region = "eu-west-2"
-    key    = "terraform.tfstate"
   }
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  profile = "default"
+  region  = "eu-west-2"
 }
