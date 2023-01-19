894cce116c8f905e36bf93303e5f5b6fe2c258a8
CloudwithNicholas <nicholasc001@hotmail.com>
2023-01-19 15:43:41 +0000

    edit api on instances type

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
+ required_providers {
+   aws = {
+     source = "hashicorp/aws"
+     version = "4.8.0"
 
-      source  = "hashicorp/aws"
-      version = "4.8.0"
-    }
-  }
+   }
+ }
+ 
+ backend "s3" {
+   region = "eu-west-2"
+   key    = "terraform.tfstate"
+ }
 }
-
+ 
 provider "aws" {
-  region = "eu-west-2"
+ region = "eu-west-2"
 }
