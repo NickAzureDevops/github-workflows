a3526f653fd1b9171b44e44b7bb6961df51c1c93
nicholaschangIT <nicholasc001@hotmail.com>
2023-01-20 21:59:02 +0000

    first commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.8.0"
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "4.8.0"
 
-    }
-  }
+#     }
+#   }
 
-  backend "s3" {
-    region = "eu-west-2"
-  }
-}
+#   backend "s3" {
+#     region = "eu-west-2"
+#   }
+# }
 
-provider "aws" {
-  region = "eu-west-2"
-}
+# provider "aws" {
+#   region = "eu-west-2"
+# }
