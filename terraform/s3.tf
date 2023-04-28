bdd06b69f08df3688f08b3c890a859e68c482482
nicholas chang <nicholasc001@hotmail.com>
2023-04-28 12:10:31 +0100

    uncomment location variable

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,13 +1,8 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket = "my-tf-test-bucket"
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
-    }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
-
-
 }
