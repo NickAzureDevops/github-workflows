143bd728b8c65421da726e79da1b5c6f1dc00d0c
nicholas chang <nicholasc001@hotmail.com>
2023-04-28 12:47:21 +0100

    updated region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,10 +12,3 @@ variable "instance_type" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
 } */
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket to create"
-  type        = string
-
-}
-
